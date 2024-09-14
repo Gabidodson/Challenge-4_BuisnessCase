e6b729170de911b3f8b386de2f352592054a4de4
Gabriela Dodson <dodsongabi@gmail.com>
2024-09-13 20:09:27 -0400

    Initialize inventory with product objects

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Create an Inventory Array of Product Objects
+let Inventory = [
+    {name: "Keyboard",price:10.00,quanity:40, LowStockLevel:15},
+    {name: "mouse",price:5.00,quanity:50, LowStockLevel:20},
+    {name: "laptop",price:1200,quanity:35, LowStockLevel:18},
+    {name: "cable",price:3.00,quanity:80, LowStockLevel:40},
+    {name: "phone",price:1000,quanity:55, LowStockLevel:21}
+];
+
